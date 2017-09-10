5e6131fd1efc1e9e3c8683702c1e9cbb033fdea4
Your name <lowejosh50@gmail.com>
2017-09-10 23:40:09 +1000

    reworked emptyTemplate as base.html and reworked signup page

--- workspace/assignment_sprint1/sprint1/urls.py
+++ workspace/assignment_sprint1/sprint1/urls.py
@@ -4,6 +4,7 @@ from sprint1 import views
 url patterns = [
         url(r'^$', views.index, name='index'),
         url(r'^register/', views.register, name='register'),
+        url(r'^signup/', views.signup, name='signup'),
 ]
 
 
--- workspace/assignment_sprint1/sprint1/views.py
+++ workspace/assignment_sprint1/sprint1/views.py
@@ -8,11 +8,26 @@ def index(request):
     # Return the template
     return render(request, 'publicMain.html', context=context_dict)
 
-# Register page view
+# --OLD--  Register page view
 def register(request):
     # Define the context of the python vars
-    context_dict = {'navBar': '<h5><a>Log in</a><br /><a>Register</a></h5>',
-                    'contentBox': '<h1>Testing</h1>',
-    }
+    context_dict = {'navBar': '<h5><a>Log in</a><br /><a>Register</a></h5>', }
     # Return the template
-    return render(request, 'emptyTemplate.html', context=context_dict)
+    return render(request, 'base.html', context=context_dict)
+
+# --NEW-- Signup page view
+def signup(request):
+    if request.method == 'POST' :
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+        else:
+            form = SignUpForm()
+        return render(request, 'signup.html', {'form': form})
+
+
