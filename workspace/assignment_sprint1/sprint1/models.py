83b0a479604b57da20d48ecc7baf3ba7874221c8
Your name <lowejosh50@gmail.com>
2017-09-11 12:03:53 +1000

    created models.py, linked django User model to custom Profile model and started to sync form data

--- workspace/assignment_sprint1/sprint1/models.py
+++ workspace/assignment_sprint1/sprint1/models.py
@@ -1,3 +1,21 @@
+# Imports
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+# Profile Model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dateOfBirth = models.DateField(null=True, blank=True)
+
+    # Define string representation
+    def __str__(self):
+        return self.user.username
+
+# Create and update user from signal
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
--- workspace/assignment_sprint1/sprint1/views.py
+++ workspace/assignment_sprint1/sprint1/views.py
@@ -12,14 +12,7 @@ def index(request):
     # Return the template
     return render(request, 'publicMain.html', context=context_dict)
 
-# --OLD--  Register page view
-def register(request):
-    # Define the context of the python vars
-    context_dict = {'navBar': '<h5><a>Log in</a><br /><a>Register</a></h5>', }
-    # Return the template
-    return render(request, 'base.html', context=context_dict)
-
-# --NEW-- Signup page view
+# Signup page view
 def signup(request):
 
     # Define the navbar
@@ -29,14 +22,21 @@ def signup(request):
     if request.method == 'POST' :
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
+
+            # Retrieve data from forms and link to the User model
+            user = form.save()
+            user.refresh_from_db()  # Loads the profile instance created from the signal
+            user.profile.dateOfBirth = form.cleaned_data.get('dateOfBirth')
+            user.save()
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+
+            # Create, login and redirect to index page
+            user = authenticate(username=user.username, password=raw_password)
             login(request, user)
             return redirect('index')
     else:
         form = SignUpForm()
+    # Render the page and set the context of python variables
     return render(request, 'signup.html', {'form': form, 'navBar' : navBar,})
 
 
