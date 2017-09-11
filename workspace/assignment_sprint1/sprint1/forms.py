a1cf17d213f7bff6d1f175e759523c6b78a0d88f
Your name <lowejosh50@gmail.com>
2017-09-11 11:12:19 +1000

    added placeholder text widget to signup form fields

--- workspace/assignment_sprint1/sprint1/forms.py
+++ workspace/assignment_sprint1/sprint1/forms.py
@@ -5,15 +5,16 @@ from django.contrib.auth.models import User
 
 # Sign up form (additional) fields
 class SignUpForm(UserCreationForm):
-    firstName = forms.CharField(required=True)   # Note for later: help_text can be an additional parameter
-    lastName = forms.CharField(required=True)
+    firstName = forms.CharField(label='First Name', required=True)   # Note for later: help_text can be an additional parameter
+    lastName = forms.CharField(label='Last Name', required=True)
     gender = forms.ChoiceField(choices=[(1, "Male"), (2, "Female"), (3, "Other")])
-    dateOfBirth = forms.DateField(required=True)
-    email = forms.EmailField(required=True)
-    phoneNumber = forms.RegexField(regex=r'^\+?1?\d{9,15}$', required=False)
-    address = forms.CharField(required=True)
+    accountType = forms.ChoiceField(label='Account Type', choices=[(1, "Student"), (2, "Business"), (3, "Tourist")])
+    dateOfBirth = forms.DateField(label='Date of Birth', required=True, widget=forms.TextInput(attrs={'placeholder': 'mm/dd/yyy'}))
+    email = forms.EmailField(label='Email Address', required=True, widget=forms.TextInput(attrs={'placeholder': 'example@email.com'}))
+    phoneNumber = forms.RegexField(label='Phone Number', regex=r'^\+?1?\d{9,15}$', required=False)
+    address = forms.CharField(label='Home Address', required=True)
 
-class Meta:
-    model = User
-    fields = ('username', 'firstName', 'lastName', 'gender', 'dateOfBirth', 'email', 'phoneNumber', 'address', 'password1', 'password2', )
+    class Meta:
+        model = User
+        fields = ('username', 'firstName', 'lastName', 'gender', 'accountType', 'email', 'dateOfBirth', 'phoneNumber', 'address', 'password1', 'password2', )
     
