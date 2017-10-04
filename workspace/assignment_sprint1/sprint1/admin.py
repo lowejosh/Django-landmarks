3ace24aefc7e7aa6853a59e5b786211652528288
Allcharles <charles@alleman.com.au>
2017-10-04 13:49:39 +1000

    Update admin.py for Admin Viewer
    Added buttons in the Admin Viewer table to allow an admin to act as a user

--- workspace/assignment_sprint1/sprint1/admin.py
+++ workspace/assignment_sprint1/sprint1/admin.py
@@ -1,5 +1,12 @@
-from django.contrib import admin
-from .models import Profile, Location, Review, Tag
+from django.contrib import admin, messages
+from django.contrib.auth import authenticate, login, update_session_auth_hash
+from .models import Profile, Location, Review, Tag, AdminViewer
+from django.utils.html import format_html
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from sprint1.models import Location, User
+from django.conf import settings
 
 # Register your models here.
 admin.site.register(Profile)
@@ -7,3 +14,17 @@ admin.site.register(Location)
 admin.site.register(Review)
 admin.site.register(Tag)
 
+def autologin(modeladmin, request, queryset):
+    if queryset.count() != 1:
+        modeladmin.message_user(request, "Cannot login into more than one account", level=messages.ERROR)
+        return
+
+    user = queryset[0].user
+    login(request, user)
+    request.session['admin'] = True
+    return redirect('index')
+
+class AdminViewerFuntion(admin.ModelAdmin):
+    actions = [autologin]
+
+admin.site.register(AdminViewer, AdminViewerFuntion)
