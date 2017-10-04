d0972305ee7d2f78d87a6114482cf7e832b87014
Misha Burkot <mikhail.burkot@connect.qut.edu.au>
2017-10-04 11:30:03 +1000

    url fixing and links to delete from modify

--- workspace/assignment_sprint1/assignment_sprint1/urls.py
+++ workspace/assignment_sprint1/assignment_sprint1/urls.py
@@ -34,5 +34,5 @@ urlpatterns = [
     url(r'^modify/edit_profile/$', views.edit_profile, name='edit_profile'),
     url(r'^modify/password/$', views.password, name='password'),
     url(r'^email/$', views.email, name='email'),
-    url(r'^del_user/$', views.del_user, name='del_user')
+    url(r'^modify/del_user/$', views.del_user, name='del_user')
 ]
