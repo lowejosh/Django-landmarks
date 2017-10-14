d923ee2cd34a22527f0a2fbc80743fe3363c8e52
charles <charles@businessinmind.com.au>
2017-10-14 14:42:24 +1000

    Finished Admin Bug Report
    Finished full functionality of the administrators bug report page.

--- workspace/assignment_sprint1/sprint1/admin.py
+++ workspace/assignment_sprint1/sprint1/admin.py
@@ -1,22 +1,28 @@
-from django.contrib import admin
-from .models import Profile, Location, Review, Tag, Map, LocationSuggestion
 from django.contrib import admin, messages
 from django.contrib.auth import authenticate, login, update_session_auth_hash
-from .models import Profile, Location, Review, Tag, AdminViewer, EmailForm, Bug
 from django.utils.html import format_html
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from sprint1.models import Location, User
 from django.conf import settings
+from .models import Profile, Location, Review, User, Tag, Map, LocationSuggestion, AdminViewer, EmailForm, Bug
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'firstName', 'lastName', 'gender', 'accountType', 'dateOfBirth', 'email', 'phoneNumber', 'address']
+# Function to allow admins to solve bugs
+def AcceptBug(modeladmin, request, queryset):
+    modeladmin.message_user(request, "You have accepted " + str(queryset.count()) + " bugs as being solved.")
+    for entry in queryset:
+        Bug.objects.filter(id=entry.id).delete();
+AcceptBug.short_description = "Accept Bugs as Solved"
 
-class ProfileBug(admin.ModelAdmin):
-    list_display = ['subject', 'description']
+# Function to allow admins to remove bugs
+def RemoveBug(modeladmin, request, queryset):
+    modeladmin.message_user(request, "You have removed " + str(queryset.count()) + " bugs.")
+    for entry in queryset:
+        Bug.objects.filter(id=entry.id).delete();
+RemoveBug.short_description = "Remove Invalid Bugs"
 
-def autologin(modeladmin, request, queryset):
+# Function to allow admins to login as the selected user
+def AutoLogin(modeladmin, request, queryset):
     if queryset.count() != 1:
         modeladmin.message_user(request, "Cannot login into more than one account", level=messages.ERROR)
         return
@@ -25,9 +31,21 @@ def autologin(modeladmin, request, queryset):
     login(request, user)
     request.session['admin'] = True
     return redirect('index')
+AutoLogin.short_description = "Login as User"
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'firstName', 'lastName', 'gender', 'accountType', 'dateOfBirth', 'email', 'phoneNumber', 'address']
+
+class ProfileBug(admin.ModelAdmin):
+    list_display = ['subject', 'description']
+    actions = [AcceptBug, RemoveBug]
+    # Remove default delete bugs action
+    del actions[0]
 
 class AdminViewerFuntion(admin.ModelAdmin):
-    actions = [autologin]
+    actions = [AutoLogin]
+    # Remove default remove user action
+    del actions[0]
 
 # Register your models here.
 admin.site.register(Profile, ProfileAdmin)
