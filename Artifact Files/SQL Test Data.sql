eb5feb17b0ff039d6377646f8ebdcb910acf8924
AshaDasha <30457605+AshaDasha@users.noreply.github.com>
2017-11-03 23:54:08 +1000

    Updated Test Data

--- Artifact Files/SQL Test Data.sql
+++ Artifact Files/SQL Test Data.sql
@@ -8,27 +8,23 @@ INSERT INTO LocationSuggestion VALUES ("Jazza's Bar and Grill", -27.475791, 153.
 
 INSERT INTO LocationSuggestion VALUES ("Timezone", -26.0, 140.05, "23 Lovely ln", "Soooo many games!", 5, "");
 
-INSERT INTO Location VALUES ("", latitude, longitude, locationAddress, locationBio, locationType, locationImagePath);
+INSERT INTO Location VALUES ("Matra Hotel", -27.478037, 153.020854, "161 Grey St, South Brisbane QLD 4101", "A luxuriant place to spend the night", 2, locationImagePath);
 
-INSERT INTO Location VALUES ("", latitude, longitude, locationAddress, locationBio, locationType, locationImagePath);
+INSERT INTO Location VALUES ("QUT Book Store", -27.477356, 153.028415, "Level 3, P Block, 2 George Street, Brisbane City QLD 4000", "Has all of your textbooks, plus some enjoyable reads!", 1, "");
 
-INSERT INTO Location VALUES ("", latitude, longitude, locationAddress, locationBio, locationType, locationImagePath);
+INSERT INTO Location VALUES ("Garden City Library", -27.562975, 153.082513, "Garden City Shopping Centre Interchange, Kessels Rd, Upper Mount Gravatt QLD 4122", "A relaxing place to read a book.", 1, "");
 
-INSERT INTO Location VALUES ("", latitude, longitude, locationAddress, locationBio, locationType, locationImagePath);
+INSERT INTO Location VALUES ("Griffith Nathan", -27.556950, 153.053445, "170 Kessels Rd, Nathan QLD 4111", locationBio, locationType, locationImagePath);
 
-INSERT INTO Profile VALUES (user, firstName, lastName, gender, accountType, dateOfBirth, email, phoneNumber, address, password);
+INSERT INTO Profile VALUES ("Kev07", "Kevin", "Rudd", "Male", "Businessman", "21/8/1957", KRudd@gmail.com, 048756576, "65 Kookaburra rd", "bigledge");
 
-INSERT INTO Profile VALUES (user, firstName, lastName, gender, accountType, dateOfBirth, email, phoneNumber, address, password);
+INSERT INTO Profile VALUES ("P3t3r94", "Peter", "Parker", "Male", "Studnet", "12/2/1994", "PParker@gmail.com", "0490902342", "12 Queen St", "ImSp1d3y");
 
-INSERT INTO Profile VALUES (user, firstName, lastName, gender, accountType, dateOfBirth, email, phoneNumber, address, password);
+INSERT INTO Profile VALUES ("Jenjer1", "Jenny", "Smith", "Female", "Tourist", "13/7/1989", "JenjerAle@gmail.com", "0489238041", "34 Pickup ln", "gingerb33r");
 
-INSERT INTO Profile VALUES (user, firstName, lastName, gender, accountType, dateOfBirth, email, phoneNumber, address, password);
+INSERT INTO Review VALUES ("Kev07", "Mantra Hotel", "Absolutely stunning view", 5);
 
-INSERT INTO Profile VALUES (user, firstName, lastName, gender, accountType, dateOfBirth, email, phoneNumber, address, password);
+INSERT INTO Review VALUES ("P3t3r94", "QUT Book Store", "Wow! So many radiation textbooks!", 4);
 
-INSERT INTO Review VALUES (user, location, reviewText, rating);
-
-INSERT INTO Review VALUES (user, location, reviewText, rating);
-
-INSERT INTO Review VALUES (user, location, reviewText, rating);
+INSERT INTO Review VALUES ("Jenjer1", "Mantra Hotel", "Eh, too expensive for me", 2);
 
