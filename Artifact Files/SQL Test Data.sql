12c69dc0d9f49735d7537ad6e2ab7aea78e2bba9
AshaDasha <30457605+AshaDasha@users.noreply.github.com>
2017-11-03 23:36:55 +1000

    More Test Data

---
+++ Artifact Files/SQL Test Data.sql
@@ -0,0 +1,34 @@
+INSERT INTO Bug VALUES (B, "The contact information bar down the bottom is transparent and makes it hard to read information on the bottom of the page.");
+
+INSERT INTO Bug VALUES (C, "I can't upload a pictur to my favourite location");
+
+INSERT INTO Bug VALUES (C, "ive sent a hundred referral links to my freind and he still hasnt got them, wtf?");
+
+INSERT INTO LocationSuggestion VALUES ("Jazza's Bar and Grill", -27.475791, 153.027826, "86 Pinhead st", "Best burgesr I've ever had.", 5, "");
+
+INSERT INTO LocationSuggestion VALUES ("Timezone", -26.0, 140.05, "23 Lovely ln", "Soooo many games!", 5, "");
+
+INSERT INTO Location VALUES ("", latitude, longitude, locationAddress, locationBio, locationType, locationImagePath);
+
+INSERT INTO Location VALUES ("", latitude, longitude, locationAddress, locationBio, locationType, locationImagePath);
+
+INSERT INTO Location VALUES ("", latitude, longitude, locationAddress, locationBio, locationType, locationImagePath);
+
+INSERT INTO Location VALUES ("", latitude, longitude, locationAddress, locationBio, locationType, locationImagePath);
+
+INSERT INTO Profile VALUES (user, firstName, lastName, gender, accountType, dateOfBirth, email, phoneNumber, address, password);
+
+INSERT INTO Profile VALUES (user, firstName, lastName, gender, accountType, dateOfBirth, email, phoneNumber, address, password);
+
+INSERT INTO Profile VALUES (user, firstName, lastName, gender, accountType, dateOfBirth, email, phoneNumber, address, password);
+
+INSERT INTO Profile VALUES (user, firstName, lastName, gender, accountType, dateOfBirth, email, phoneNumber, address, password);
+
+INSERT INTO Profile VALUES (user, firstName, lastName, gender, accountType, dateOfBirth, email, phoneNumber, address, password);
+
+INSERT INTO Review VALUES (user, location, reviewText, rating);
+
+INSERT INTO Review VALUES (user, location, reviewText, rating);
+
+INSERT INTO Review VALUES (user, location, reviewText, rating);
+
